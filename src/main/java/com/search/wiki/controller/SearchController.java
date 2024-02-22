b46311e90279b520d030de3c4f4eaaee155595cd
Hannah-Kaliada <120135661+Hannah-Kaliada@users.noreply.github.com>
2024-02-22 19:27:23 +0300

    Adding new files

--- src/main/java/com/search/wiki/controller/SearchController.java
+++ src/main/java/com/search/wiki/controller/SearchController.java
@@ -1,4 +1,17 @@
 package com.search.wiki.controller;
 
+import com.search.wiki.model.SearchResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/search")
 public class SearchController {
+    @GetMapping("/getSearchResult")
+    public SearchResult getSearchResult(@RequestParam String word)
+    {
+        return SearchResult.builder().query("That's hard coded JSON result: "+word).build();
+    }
 }
--- src/main/java/com/search/wiki/model/SearchResult.java
+++ src/main/java/com/search/wiki/model/SearchResult.java
@@ -1,4 +1,10 @@
 package com.search.wiki.model;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class SearchResult {
+    private String query;
 }
