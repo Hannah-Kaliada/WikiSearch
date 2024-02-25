62eceab66e304e6819804e8fe87032fb80b71e48
Hannah-Kaliada <120135661+Hannah-Kaliada@users.noreply.github.com>
2024-02-25 21:00:09 +0300

    Working lab1

--- src/main/java/com/search/wiki/controller/SearchController.java
+++ src/main/java/com/search/wiki/controller/SearchController.java
@@ -3,7 +3,6 @@ package com.search.wiki.controller;
 import com.search.wiki.model.SearchResult;
 import com.search.wiki.service.WikiService;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
