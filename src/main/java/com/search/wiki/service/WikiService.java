9a85c8ed99394fd4e619531ceb30c5818c8bd4be
Hannah-Kaliada <120135661+Hannah-Kaliada@users.noreply.github.com>
2024-02-25 21:09:44 +0300

    Working lab1

--- src/main/java/com/search/wiki/service/WikiService.java
+++ src/main/java/com/search/wiki/service/WikiService.java
@@ -1,7 +1,6 @@
 package com.search.wiki.service;
 
 import com.search.wiki.model.SearchResult;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestParam;
 
--- src/main/java/com/search/wiki/service/implementation/SearchQueryImplementation.java
+++ src/main/java/com/search/wiki/service/implementation/SearchQueryImplementation.java
@@ -1,12 +1,10 @@
-package com.search.wiki.service.Implemention;
+package com.search.wiki.service.implementation;
 
 import com.search.wiki.model.SearchResult;
 import com.search.wiki.service.WikiService;
 import org.springframework.stereotype.Service;
-
 @Service
-public class SearchQueryImplemention implements WikiService
-{
+public class SearchQueryImplementation implements WikiService {
     @Override
     public SearchResult getSearchResult(String word) {
         return SearchResult.builder().query("That's hard coded JSON result: "+word).build();
