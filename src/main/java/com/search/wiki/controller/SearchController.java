721efdd409826e9a98fbb8834e63cf336e7cbedf
Hannah-Kaliada <120135661+Hannah-Kaliada@users.noreply.github.com>
2024-02-25 21:04:19 +0300

    Working lab1

--- src/main/java/com/search/wiki/controller/SearchController.java
+++ src/main/java/com/search/wiki/controller/SearchController.java
@@ -3,6 +3,7 @@ package com.search.wiki.controller;
 import com.search.wiki.model.SearchResult;
 import com.search.wiki.service.WikiService;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/v1/search")
 @AllArgsConstructor
 public class SearchController {
+    @Qualifier("searchQueryImplemention")
     private final WikiService service;
 
     @GetMapping("/getSearchResult")
--- src/main/java/com/search/wiki/service/WikiService.java
+++ src/main/java/com/search/wiki/service/WikiService.java
@@ -1,6 +1,7 @@
 package com.search.wiki.service;
 
 import com.search.wiki.model.SearchResult;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestParam;
 
