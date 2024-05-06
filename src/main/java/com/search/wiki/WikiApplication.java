1e1ee1285165cb3cfa071b6c5f071d76f4cb872e
Hannah-Kaliada <120135661+Hannah-Kaliada@users.noreply.github.com>
2024-05-06 14:38:57 +0300

    Trying to deploy

--- src/main/java/com/search/wiki/WikiApplication.java
+++ src/main/java/com/search/wiki/WikiApplication.java
@@ -2,8 +2,6 @@ package com.search.wiki;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.server.WebServerFactoryCustomizer;
-import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -27,15 +25,6 @@ public class WikiApplication {
       @Override
       public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**").allowedOrigins("*");
-      }
-    };
-  }
-  @Bean
-  public WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> webServerFactoryCustomizer() {
-    return factory -> {
-      String port = System.getenv("PORT");
-      if (port != null) {
-        factory.setPort(Integer.parseInt(port));
       }
     };
   }
